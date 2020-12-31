9990eda8172d28d8136d0a257012a0744154e745
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2020-12-31 01:20:41 -0500

    Added incrementing element.

---
+++ script.js
@@ -0,0 +1,24 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = Number(counter.getAttribute('data-target'))
+        // console.log(typeof target, target)
+        const c = Number(counter.innerText)
+
+        const increment = target / 300
+
+        // console.log(increment)
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            coounter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
